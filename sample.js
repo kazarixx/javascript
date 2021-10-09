db39161322972272fee6e47820819b26b7585ef3
かざり <kazari@kazarinoMacBook-Air.local>
2021-10-10 00:23:23 +0900

    [start]じゃんけん

---
+++ sample.js
@@ -0,0 +1,17 @@
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+
+alert('あなたの選んだ手は' + user_hand + 'です。');
+
+function getJShand(){
+  let js_hand_num = Math.floor( Math.random() * 3 );
+/Math.floor（数値）で、数値の少数点以下の切り捨て処理/ 
+  if(js_hand_num == 0){
+    js_hand = "グー";
+  } else if(js_hand_num == 1){
+    js_hand = "チョキ";
+  } else if(js_hand_num == 2){
+    js_hand = "パー";
+  }
+
+  return js_hand;
+}
