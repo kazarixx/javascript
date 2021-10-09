048137f13a3f3e20f3a6bfb051d31f3fa74e9288
かざり <kazari@kazarinoMacBook-Air.local>
2021-10-10 01:06:59 +0900

    [finish]じゃんけん演習

--- sample.js
+++ sample.js
@@ -1,17 +1,62 @@
+// じゃんけんの手を入力してもらうプロンプト欄を生成
 let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
 
-alert('あなたの選んだ手は' + user_hand + 'です。');
+// じゃんけんの手をランダムに作成する関数を呼び出す
+let js_hand = getJShand();
 
+// ユーザの手とJavaScriptのじゃんけんの手を比べる関数を呼び出し、結果をjudgeに入れる
+let judge = winLose(user_hand, js_hand);
+
+// 結果を表示する
+alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+
+// ランダムでじゃんけんの手を作成する関数
 function getJShand(){
   let js_hand_num = Math.floor( Math.random() * 3 );
-/Math.floor（数値）で、数値の少数点以下の切り捨て処理/ 
+  / Math.floor（数値）で、数値の少数点以下の切り捨て処理/ 
+  //Math.random()は、0〜0.9999...(1未満)の間から、1つの数値をランダムで返す。0は含むが、1は含まない。/
+  let hand_name;
+
   if(js_hand_num == 0){
-    js_hand = "グー";
+    hand_name = "グー";
   } else if(js_hand_num == 1){
-    js_hand = "チョキ";
+    hand_name = "チョキ";
   } else if(js_hand_num == 2){
-    js_hand = "パー";
+    hand_name = "パー";
   }
 
-  return js_hand;
+  return hand_name;
+}
+
+// ユーザの手とJavaScriptのじゃんけんの手を比べる関数
+function winLose(user, js){
+  let winLoseStr;
+
+  if(user == "グー"){
+    if(js == "グー"){
+      winLoseStr = "あいこ";
+    } else if(js == "チョキ"){
+      winLoseStr = "勝ち";
+    } else if(js == "パー"){
+      winLoseStr = "負け";
+    }
+  } else if(user == "チョキ"){
+    if(js == "グー"){
+      winLoseStr = "負け";
+    } else if(js == "チョキ"){
+      winLoseStr = "あいこ";
+    } else if(js == "パー"){
+      winLoseStr = "勝ち";
+    }
+  } else if(user == "パー"){
+    if(js == "グー"){
+      winLoseStr = "勝ち";
+    } else if(js == "チョキ"){
+      winLoseStr = "負け";
+    } else if(js == "パー"){
+      winLoseStr = "あいこ";
+    }
+  }
+
+  return winLoseStr;
 }
